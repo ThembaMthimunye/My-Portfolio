50116858103a952ed5002dc4d2fa709ca2e014b0
ThembaMthimunye <218634567@tut4life.ac.za>
2024-09-19 13:31:19 +0200

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./Components/NAVBAR/Navbar";
+import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
 import Skills from "./Components/Skills/Skills";
